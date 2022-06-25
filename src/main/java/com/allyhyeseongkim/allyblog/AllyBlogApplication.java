8c417d641237a58c7b8dcaf5fa3dba7b9743094b
AllyHyeseongKim <ally3k@icloud.com>
2022-06-25 22:39:12 +0900

    [develop] Initialize Spring Boot Application
    *

---
+++ src/main/java/com/allyhyeseongkim/allyblog/AllyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.allyhyeseongkim.allyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllyBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allyhyeseongkim/allyblog/AllyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allyhyeseongkim.allyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
