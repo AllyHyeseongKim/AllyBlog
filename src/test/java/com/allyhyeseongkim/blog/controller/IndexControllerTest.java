934445fe4f6f8dfce6f5f97c9d7d720500d1472e
AllyHyeseongKim <ally3k@icloud.com>
2023-09-29 13:33:17 +0900

    Convert html to mustache
    src/main/resources/templates
    src/main/resources/static/css

---
+++ src/test/java/com/allyhyeseongkim/blog/controller/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.allyhyeseongkim.blog.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void loadMainPage() {
+        //given
+        //when
+        String actual = restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(actual).contains("안녕하세요.");
+    }
+
+}
